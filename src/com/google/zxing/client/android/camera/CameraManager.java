2e8338630ad5a74cbf97a1baa905e1d6f28095cd
dkgp <david.kwak@adp.com>
2013-11-20 10:41:48 -0600

    increased scan width

--- src/com/google/zxing/client/android/camera/CameraManager.java
+++ src/com/google/zxing/client/android/camera/CameraManager.java
@@ -39,7 +39,7 @@ public final class CameraManager {
 
   private static final String TAG = CameraManager.class.getSimpleName();
 
-  private static final int MIN_FRAME_WIDTH = 240;
+  private static final int MIN_FRAME_WIDTH = 960;// 240; djk increased to read vin
   private static final int MIN_FRAME_HEIGHT = 240;
   private static final int MAX_FRAME_WIDTH = 960; // = 1920/2
   private static final int MAX_FRAME_HEIGHT = 540; // = 1080/2
